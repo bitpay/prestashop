3bedbc66f8ff4a7bd766952d77653f3478e82790
Joshua Lewis <joshlewis@gmail.com>
2019-05-09 14:54:41 -0400

    ipn update

--- controllers/front/bitpayipn.php
+++ controllers/front/bitpayipn.php
@@ -10,8 +10,35 @@ if (isset($_POST)):
     $transaction_status = $event['name'];
 
     $table_name = '_bitpay_checkout_transactions';
-    $order_table = $db_prefix.'orders';
-    $order_history_table = $db_prefix.'order_history';
+    $order_table = $db_prefix . 'orders';
+    $order_history_table = $db_prefix . 'order_history';
+
+    #load BP classess
+    $level = 2;
+    include dirname(__DIR__, $level) . "/BitPayLib/BPC_Client.php";
+    include dirname(__DIR__, $level) . "/BitPayLib/BPC_Configuration.php";
+    include dirname(__DIR__, $level) . "/BitPayLib/BPC_Invoice.php";
+    include dirname(__DIR__, $level) . "/BitPayLib/BPC_Item.php";
+
+    #BITPAY SPECIFIC INFO
+
+    $env = 'test';
+    $bitpay_token = Configuration::get('bitpay_checkout_token_dev');
+
+    if (Configuration::get('bitpay_checkout_endpoint') == 1):
+        $env = 'production';
+        $bitpay_token = get_option('bitpay_checkout_token_prod');
+    endif;
+
+    $config = new BPC_Configuration($bitpay_token, $env);
+    $params = new stdClass();
+
+    $params->invoiceID = $transaction_id;
+
+    $item = new BPC_Item($config, $params);
+
+    $invoice = new BPC_Invoice($item); //this creates the invoice with all of the config params
+    $orderStatus = json_decode($invoice->BPC_checkInvoiceStatus($transaction_id));
 
     $bp_sql = "SELECT * FROM $table_name WHERE transaction_id = '$transaction_id'";
     $results = Db::getInstance()->executes($bp_sql);
@@ -20,66 +47,88 @@ if (isset($_POST)):
 
         switch ($transaction_status) {
             case 'invoice_confirmed': #complete
-                #update the order and history
-                $current_state = 2;
-                $db = Db::getInstance();
-                $bp_u = "UPDATE $order_table SET current_state = $current_state WHERE id_order = '$orderId'";
-                $db->Execute($bp_u);
+                if ($orderStatus->data->status == 'confirmed'):
+                    #update the order and history
+                    $current_state = 2;
+                    $db = Db::getInstance();
+                    $bp_u = "UPDATE $order_table SET current_state = $current_state WHERE id_order = '$orderId'";
+                    $db->Execute($bp_u);
 
-                #update the transaction table
-                $bp_t = "UPDATE $table_name SET transaction_status = '$transaction_status' WHERE transaction_id = '$transaction_id' AND order_id = $orderId";
-                $db->Execute($bp_t);
+                    #update the transaction table
+                    $bp_t = "UPDATE $table_name SET transaction_status = '$transaction_status' WHERE transaction_id = '$transaction_id' AND order_id = $orderId";
+                    $db->Execute($bp_t);
 
-                #update the history table
-                $order_history_table = 'ps_order_history';
-                $bp_h = "INSERT INTO $order_history_table (id_employee,id_order,id_order_state,date_add)
-                                        VALUES (0,'$orderId',$current_state,NOW())";
-                $db->Execute($bp_h);
-
-
+                    #update the history table
+                    $order_history_table = 'ps_order_history';
+                    $bp_h = "INSERT INTO $order_history_table (id_employee,id_order,id_order_state,date_add)
+					         VALUES (0,'$orderId',$current_state,NOW())";
+                    $db->Execute($bp_h);
+                endif;
                 break;
 
             case 'invoice_paidInFull': #pending
-            #update the order and history
-            $current_state = 3;
-            $db = Db::getInstance();
-            $bp_u = "UPDATE $order_table SET current_state = $current_state WHERE id_order = '$orderId'";
-            $db->Execute($bp_u);
+                #update the order and history
+                if ($orderStatus->data->status == 'paid'):
+                    $current_state = 3;
+                    $db = Db::getInstance();
+                    $bp_u = "UPDATE $order_table SET current_state = $current_state WHERE id_order = '$orderId'";
+                    $db->Execute($bp_u);
 
-            #update the transaction table
-            $bp_t = "UPDATE $table_name SET transaction_status = '$transaction_status' WHERE transaction_id = '$transaction_id' AND order_id = $orderId";
-            $db->Execute($bp_t);
+                    #update the transaction table
+                    $bp_t = "UPDATE $table_name SET transaction_status = '$transaction_status' WHERE transaction_id = '$transaction_id' AND order_id = $orderId";
+                    $db->Execute($bp_t);
 
-            #update the history table
-            $order_history_table = 'ps_order_history';
-            $bp_h = "INSERT INTO $order_history_table (id_employee,id_order,id_order_state,date_add)
-                                    VALUES (0,'$orderId',$current_state,NOW())";
-            $db->Execute($bp_h);
-
+                    #update the history table
+                    $order_history_table = 'ps_order_history';
+                    $bp_h = "INSERT INTO $order_history_table (id_employee,id_order,id_order_state,date_add)
+					        VALUES (0,'$orderId',$current_state,NOW())";
+                    $db->Execute($bp_h);
+                endif;
                 break;
 
             case 'invoice_failedToConfirm':
-            #update the order and history
-            $current_state = 8;
-            $db = Db::getInstance();
-            $bp_u = "UPDATE $order_table SET current_state = $current_state WHERE id_order = '$orderId'";
-            $db->Execute($bp_u);
+                #update the order and history
+                if ($orderStatus->data->status == 'invalid'):
+                    $current_state = 8;
+                    $db = Db::getInstance();
+                    $bp_u = "UPDATE $order_table SET current_state = $current_state WHERE id_order = '$orderId'";
+                    $db->Execute($bp_u);
 
-            #update the transaction table
-            $bp_t = "UPDATE $table_name SET transaction_status = '$transaction_status' WHERE transaction_id = '$transaction_id' AND order_id = $orderId";
-            $db->Execute($bp_t);
+                    #update the transaction table
+                    $bp_t = "UPDATE $table_name SET transaction_status = '$transaction_status' WHERE transaction_id = '$transaction_id' AND order_id = $orderId";
+                    $db->Execute($bp_t);
 
-            #update the history table
-            $order_history_table = 'ps_order_history';
-            $bp_h = "INSERT INTO $order_history_table (id_employee,id_order,id_order_state,date_add)
-                                    VALUES (0,'$orderId',$current_state,NOW())";
-            $db->Execute($bp_h);
-
+                    #update the history table
+                    $order_history_table = 'ps_order_history';
+                    $bp_h = "INSERT INTO $order_history_table (id_employee,id_order,id_order_state,date_add)
+					         VALUES (0,'$orderId',$current_state,NOW())";
+                    $db->Execute($bp_h);
+                endif;
                 break;
             case 'invoice_expired':
                 //delete the previous order
                 #update the order and history
-                $current_state = 6;
+                if ($orderStatus->data->status == 'expired'):
+                    $current_state = 6;
+                    $db = Db::getInstance();
+                    $bp_u = "UPDATE $order_table SET current_state = $current_state WHERE id_order = '$orderId'";
+                    $db->Execute($bp_u);
+
+                    #update the transaction table
+                    $bp_t = "UPDATE $table_name SET transaction_status = '$transaction_status' WHERE transaction_id = '$transaction_id' AND order_id = $orderId";
+                    $db->Execute($bp_t);
+
+                    #update the history table
+                    $order_history_table = 'ps_order_history';
+                    $bp_h = "INSERT INTO $order_history_table (id_employee,id_order,id_order_state,date_add)
+					         VALUES (0,'$orderId',$current_state,NOW())";
+                    $db->Execute($bp_h);
+                endif;
+                break;
+
+            case 'invoice_refundComplete':
+                #update the order and history
+                $current_state = 7;
                 $db = Db::getInstance();
                 $bp_u = "UPDATE $order_table SET current_state = $current_state WHERE id_order = '$orderId'";
                 $db->Execute($bp_u);
@@ -91,27 +140,8 @@ if (isset($_POST)):
                 #update the history table
                 $order_history_table = 'ps_order_history';
                 $bp_h = "INSERT INTO $order_history_table (id_employee,id_order,id_order_state,date_add)
-                                        VALUES (0,'$orderId',$current_state,NOW())";
+				         VALUES (0,'$orderId',$current_state,NOW())";
                 $db->Execute($bp_h);
-
-                break;
-
-            case 'invoice_refundComplete':
-            #update the order and history
-            $current_state = 7;
-            $db = Db::getInstance();
-            $bp_u = "UPDATE $order_table SET current_state = $current_state WHERE id_order = '$orderId'";
-            $db->Execute($bp_u);
-
-            #update the transaction table
-            $bp_t = "UPDATE $table_name SET transaction_status = '$transaction_status' WHERE transaction_id = '$transaction_id' AND order_id = $orderId";
-            $db->Execute($bp_t);
-
-            #update the history table
-            $order_history_table = 'ps_order_history';
-            $bp_h = "INSERT INTO $order_history_table (id_employee,id_order,id_order_state,date_add)
-                                    VALUES (0,'$orderId',$current_state,NOW())";
-            $db->Execute($bp_h);
 
                 break;
 
